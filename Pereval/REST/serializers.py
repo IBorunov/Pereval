60daa653bba33e0c5915f5b1faa54f5ca975a60e
IBorunov <ivanborun@gmail.com>
2024-04-23 22:19:13 +0300

    serializers prototype added

---
+++ Pereval/REST/serializers.py
@@ -0,0 +1,43 @@
+from .models import User, PerevalAdded, Level, Coord, Image
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+from drf_writable_nested.mixins import UniqueFieldsMixin
+from rest_framework import serializers
+
+
+class UserSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'fam', 'name', 'otc', 'phone']
+
+    def create(self, validated_data):
+        user, created = User.objects.get_or_create(**validated_data)
+        return user
+
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        fields = '__all__'
+
+
+class CoordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coord
+        fields = '__all__'
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '__all__'
+
+
+class PerevalSerializer(WritableNestedModelSerializer):
+    user = UserSerializer()
+    coords = CoordSerializer()
+    level = LevelSerializer()
+    image = ImageSerializer()
+
+    class Meta:
+        model = PerevalAdded
+        fields = ['id', 'beautyTitle', 'title', 'other_titles', 'connect', 'add_time', 'user', 'coord', 'level', 'image']
