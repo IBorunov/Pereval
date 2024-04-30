811a1bf97e9530cf810a72210e41283cf25321c1
IBorunov <ivanborun@gmail.com>
2024-04-30 10:12:37 +0300

    new API to get pereval by ID added

--- Pereval/Pereval/urls.py
+++ Pereval/Pereval/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from REST.views import SubmitData
+from REST.views import SubmitData, RetrievePerevalByID
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/submitdata', SubmitData.as_view())
+    path('api/v1/submitdata', SubmitData.as_view()),
+    path('api/v1/retrieve/<int:pk>', RetrievePerevalByID.as_view()),
 ]
--- Pereval/REST/views.py
+++ Pereval/REST/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
 
 from REST.models import PerevalAdded
@@ -39,3 +39,7 @@ class SubmitData(CreateAPIView):
             }
             return Response(response_data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
+class RetrievePerevalByID(RetrieveAPIView):
+    queryset = PerevalAdded.objects.all()
+    serializer_class = PerevalSerializer
